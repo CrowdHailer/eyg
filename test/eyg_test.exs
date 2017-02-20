87b8a366251c3ec308805cf721bd09006db170f5
CrowdHailer <peterhsaxton@gmail.com>
2017-02-20 22:54:33 +0000

    some manipulations

--- test/eyg_test.exs
+++ test/eyg_test.exs
@@ -1,8 +1,30 @@
+defmodule Mack do
+  defmacro hi(a, b) do
+    IO.inspect(a)
+    quote do: unquote(b)
+  end
+end
 defmodule EygTest do
   use ExUnit.Case
   doctest Eyg
 
+  require Mack
+
+  ast = quote do: 5 + 5
+  IO.inspect(ast)
+  Macro.prewalk(ast, fn (t) -> IO.inspect(t) end)
+  modi = Macro.postwalk(ast, fn
+    # (5) -> IO.inspect(10)
+    (t) -> IO.inspect(t)
+  end)
+  |> IO.inspect
+  Module.eval_quoted(__MODULE__, modi)
+  |> IO.inspect
+  import Mack
+
   test "the truth" do
-    assert 1 + 1 == 2
+    ast = quote do: Mack.hi(1, Mack.hi(2, 3))
+    Macro.expand_once(ast, __ENV__)
+    |> IO.inspect
   end
 end
