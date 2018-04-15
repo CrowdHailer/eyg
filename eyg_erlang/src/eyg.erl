a9e3b5c2f9962cb103811989303ce9ac1c760209
CrowdHailer <peterhsaxton@gmail.com>
2018-04-15 14:37:17 +0100

    some erlang code

--- eyg_erlang/src/eyg.erl
+++ eyg_erlang/src/eyg.erl
@@ -1,13 +1,30 @@
 -module(eyg).
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 %% API exports
--export([]).
+-export([check/1]).
+
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
+check(AST) ->
+  {ok, infer_type(AST)}.
+
+infer_type(X) when is_integer(X) ->
+  {integer, {X, X}}.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+
+simple_integer_test() ->
+  ?assertMatch({ok, {integer, {3, 3}}}, check(3)).
+
+simple_addition_test() ->
+  ?assertMatch({ok, {integer, {5, 5}}}, check({add, {2, 3}})).
