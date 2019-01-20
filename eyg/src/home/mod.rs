6f3a6c9c6b23c751f0ba4b61680a6788812ee0a3
CrowdHailer <peterhsaxton@gmail.com>
2019-01-20 11:21:27 +0000

    Include in the Handler trait a next type

--- eyg/src/home/mod.rs
+++ eyg/src/home/mod.rs
@@ -11,12 +11,15 @@ pub trait Deliverable<S> {
 pub type Mail<S> = Vec<Box<dyn Deliverable<S>>>;
 
 pub trait Handler<M, S> {
-    fn handle(self, message: M) -> (Mail<S>, Self);
+    type Next;
+    fn handle(self, message: M) -> (Mail<S>, Self::Next);
 }
 
 // Expose that there are Actors or no useful insights
 // Home is the same as System or Environment or Rave or Workshop or Village,
 // Workshop is interesting as Home could become skill/capability
+// If this is a Home to these kind of workers if must be possible to insert them.
+// It's up to some other step to check that if state changes stuff still works
 pub trait Home<A, M>: Sized {
     type Worker: Handler<M, Self>;
     // I don't think this is very ergonomic Rust
@@ -24,7 +27,18 @@ pub trait Home<A, M>: Sized {
     fn put(self, address: A, worker: Self::Worker) -> Self;
 }
 
-impl<A, M, S> Deliverable<S> for Envelope<A, M> where S: Home<A, M> {
+// The specification of W here indicates this only applies for when a handler does not change the state of the worker
+// impl<A, M, S, W> Deliverable<S> for Envelope<A, M>
+//     where
+//         S: Home<A, M, Worker=W>,
+//         W: Handler<M, S, Next=S::Worker>
+// {
+impl<A, M, S> Deliverable<S> for Envelope<A, M>
+    where
+        S: Home<A, M>,
+        S::Worker: Handler<M, S, Next=S::Worker>
+        // W: Handler<M, S, Next=S::Worker>
+{
     fn deliver(self: Box<Self>, system: S) -> (Mail<S>, S) {
         let Envelope{address, message} = *self;
         let (worker, system) = system.pop(&address);
@@ -46,8 +60,9 @@ mod tests {
         pub struct Counter(i32);
         // use unit as tick for countring
         impl<S> Handler<(), S> for Counter {
-            fn handle(self, _: ()) -> (Mail<S>, Self) {
-                (vec![], Counter(self.0 + 1))
+            type Next = i32;
+            fn handle(self, _: ()) -> (Mail<S>, i32) {
+                (vec![], self.0 + 1)
             }
         }
         impl Home<String, ()> for HashMap<String, Counter> {
